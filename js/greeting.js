077e48372d5141740fbe083dc8f7172a3409dddf
chanbak <dev.chan.8888@gmail.com>
2022-12-01 00:09:53 +0900

    Devide JS files by functions

--- js/greeting.js
+++ js/greeting.js
@@ -25,22 +25,3 @@ if (savedUsername === null) {
 } else {
   paintGreeting(savedUsername);
 }
-
-const clock = document.querySelector('#clock');
-const calendar = document.querySelector('#calendar');
-
-function getTime() {
-  const time = new Date();
-  const month = String(time.getMonth()).padStart(2, '0');
-  const date = String(time.getDate()).padStart(2, '0');
-
-  const hours = String(time.getHours()).padStart(2, '0');
-  const minutes = String(time.getMinutes()).padStart(2, '0');
-  const seconds = String(time.getSeconds()).padStart(2, '0');
-
-  calendar.innerText = `${month}월 ${date}일`;
-  clock.innerText = `${hours}:${minutes}:${seconds}`;
-}
-
-getTime();
-setInterval(getTime, 1000);
