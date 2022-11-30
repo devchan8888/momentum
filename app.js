08e62373ed21532584a5892888f09f2394200d87
chanbak <dev.chan.8888@gmail.com>
2022-12-01 00:05:19 +0900

    Add Clock and Calendar

--- app.js
+++ app.js
@@ -25,3 +25,22 @@ if (savedUsername === null) {
 } else {
   paintGreeting(savedUsername);
 }
+
+const clock = document.querySelector('#clock');
+const calendar = document.querySelector('#calendar');
+
+const time = new Date();
+
+function getTime() {
+  const month = String(time.getMonth()).padStart(2, '0');
+  const date = String(time.getDate()).padStart(2, '0');
+
+  const hours = String(time.getHours()).padStart(2, '0');
+  const minutes = String(time.getMinutes()).padStart(2, '0');
+  const seconds = String(time.getSeconds()).padStart(2, '0');
+
+  calendar.innerText = `${month}월 ${date}일`;
+  clock.innerText = `${hours}:${minutes}:${seconds}`;
+}
+
+getTime();
