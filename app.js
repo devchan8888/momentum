cf3927f2d8d89b2ff808e80f12cb9e7e2e7dd9cf
chanbak <dev.chan.8888@gmail.com>
2022-11-30 23:22:36 +0900

    Change Code format and Add gitignore

--- app.js
+++ app.js
@@ -1,23 +1,17 @@
-const loginForm = document.querySelector("#login-form");
-const loginInput = document.querySelector("#login-form input")
-const greeting = document.querySelector("#greeting")
-const link = document.querySelector("a");
+const loginForm = document.querySelector('#login-form');
+const loginInput = document.querySelector('#login-form input');
+const greeting = document.querySelector('#greeting');
+const link = document.querySelector('a');
 
-const HIDDEN_CLASSNAME = 'hidden'
+const HIDDEN_CLASSNAME = 'hidden';
 
-function onLoginSubmit(event){
-    event.preventDefault();
-    const username = loginInput.value;
-    loginForm.classList.add(HIDDEN_CLASSNAME)
-    localStorage.setItem("username", username)
-    greeting.innerText = `Hello ${username}`
-    greeting.classList.remove(HIDDEN_CLASSNAME)
-}
- 
-function handleLinkClick(event){
-    event.preventDefault()
-    alert("clicked")
+function onLoginSubmit(event) {
+  event.preventDefault();
+  const username = loginInput.value;
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+  localStorage.setItem('username', username);
+  greeting.innerText = `Hello ${username}`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
-loginForm.addEventListener("submit", onLoginSubmit)
-link.addEventListener("click", handleLinkClick)
+loginForm.addEventListener('submit', onLoginSubmit);
