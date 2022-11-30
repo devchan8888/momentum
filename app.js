294e8cc91bf7efe8e606ef5f7a98c2d0407228f3
chanbak <dev.chan.8888@gmail.com>
2022-11-30 23:32:08 +0900

    Update Greeting App

--- app.js
+++ app.js
@@ -1,17 +1,27 @@
 const loginForm = document.querySelector('#login-form');
 const loginInput = document.querySelector('#login-form input');
 const greeting = document.querySelector('#greeting');
-const link = document.querySelector('a');
 
 const HIDDEN_CLASSNAME = 'hidden';
+const USERNAME_KEY = 'username';
 
 function onLoginSubmit(event) {
   event.preventDefault();
-  const username = loginInput.value;
+  const typedUsername = loginInput.value;
+  localStorage.setItem(USERNAME_KEY, typedUsername);
   loginForm.classList.add(HIDDEN_CLASSNAME);
-  localStorage.setItem('username', username);
-  greeting.innerText = `Hello ${username}`;
+  paintGreeting(typedUsername);
+}
+
+function paintGreeting(name) {
+  greeting.innerText = `Hello ${name}`;
   greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
-loginForm.addEventListener('submit', onLoginSubmit);
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+if (savedUsername === null) {
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener('submit', onLoginSubmit);
+} else {
+  paintGreeting(savedUsername);
+}
